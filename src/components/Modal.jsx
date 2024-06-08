fc533811e0ef569ff24f8c737793c8ae208f3cdc
Abdul Mateen <121253565+AbdulMateen18@users.noreply.github.com>
2024-06-08 20:39:20 +0500

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -12,7 +12,7 @@ function Modal({
   return (
     <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[70vw] bg-white rounded shadow-xl z-10">
       <span
-        className="absolute right-[0.1rem] text-[2.5rem] text-grey-800 cursor-pointer border-none bg-none"
+        className="absolute top-[-0.7rem] right-[0.1rem] text-[2.5rem] text-grey-800 cursor-pointer border-none bg-none"
         onClick={onClose}
       >
         &times;
